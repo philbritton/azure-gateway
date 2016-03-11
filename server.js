27a85ea2074f72de3646eac001e627e7929887bc
Iggy Coloma <iggy.coloma@gmail.com>
2016-03-10 17:32:10 -0800

    Update for Azure idiosyncracies.

--- server.js
+++ server.js
@@ -8,13 +8,6 @@ const morgan = require('morgan');
 const app = express();
 app.use(morgan('combined')); // comprehensive HTTP logging
 
-// Redirect all http traffic to https.
-app.use((req, res, next) => {
-  res.setHeader('Strict-Transport-Security', 'max-age=8640000; includeSubDomains');
-  if (req.headers['x-forwarded-proto'] !== 'https') { return res.redirect(301, `https://${req.headers.host}/`); }
-  return next();
-});
-
 /* eslint-disable no-process-env */
 const proxyTarget = httpProxy.createProxyServer({ target: process.env.PROXY_TARGET, changeOrigin: true });
 /* eslint-enable no-process-env */
